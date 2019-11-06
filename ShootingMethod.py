54df6208c397c48025eabf83b912323af8fef67a
Alexander Fisher <af17918@bristol.ac.uk>
2019-11-06 15:33:44 +0000

    remove code not using anymore and changed arrange to linspace

--- ShootingMethod.py
+++ ShootingMethod.py
@@ -13,27 +13,46 @@ from scipy.optimize import fsolve
 
 # residue function to be fsolved. 
 def res_fun(u):
-	x0 = u[0]
-	y0 = u[1]
-	T = u[2]
+	x0, y0, T = u
 	
 	t = np.linspace(0, T,1000)
 	sol_ode = odeint(F,[x0,y0],t,args=(1,0.1,0.2))
 	
-	sol = np.zeros(3)
-	sol[0] = x0 - sol_ode[-1,0]
-	sol[1] = y0 - sol_ode[-1,1]
-	sol[2] = x0*(1-x0)-(x0*y0)/(0.1+x0) 
-
+	sol = np.zeros(3) 
+	sol[0] = abs(x0 - sol_ode[-1,0])
+	sol[1] = abs(y0 - sol_ode[-1,1])
+	sol[2] = x0*(1-x0)-(x0*y0)/(0.1+x0)
 
 	return sol
 
 
-solution = fsolve(res_fun, [0.4,0.4,20])
 
-print(solution)
-print(res_fun(solution))
+def solve_bvp(u):
+	x0, y0, T= u
+	xs0, ys0, Ts = fsolve(res_fun, [x0, y0, T])
 
-x = [solution[0],solution[1]]
-plot(x)
+	t = np.linspace(0,Ts,1000)
+	sol_ode = odeint(F,[xs0,ys0],t,args=(1,0.1,0.2))
+	xf = sol_ode[-1,0]
+	yf = sol_ode[-1,1]
+
+	return xs0, ys0, xf, yf, Ts
+
+xs0, ys0, xf, yf, Ts = solve_bvp([0.5,0.2,25])
+print(xs0, ys0, xf, yf, Ts)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
