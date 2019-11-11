2577bdafdc382e4af41c1974b5b6327fa9797af0
Alexander Fisher <af17918@bristol.ac.uk>
2019-11-11 22:17:07 +0000

    completed hopf_bifurcation test - test 1

--- test.py
+++ test.py
@@ -1,53 +1,52 @@
 """
 Test Script:
 
-This is a script designed to test and run the shooting code in shooting.py
-through multiple differnt tests. They include testing against ode systems with known analytic solutions. 
+This is a script designed to test the shooting code in shooting.py
+for diffent types of inputs and to see accuacry and effectivess as well. This will include testing against ode systems with known analytic solutions.
+In addition erros messages and raises will also be tested.  
 
 """
 
-import shootingmethod
-import unittest
+import shootingmethod as sm
+from math import pi, sqrt
+import numpy as np
 
 
-"""
-Create inputs 
+# Shooting test given aanalytical solutions:
 
-Execute the code being tested, capturing the output
+def test_hopf_bifurcation(b0,bf):
 
-Compare the output with an expected result
+		def hopf_ode(u0,t,b,a):
+			x,y = u0
+			dxdt = b*x-y+a*x*(x**2+y**2)
+			dydt = x+b*y+a*y*(x**2+y**2)
+			return [dxdt,dydt]
 
-first set:
+		def simulation(b,a):
+			sol = sm.shooting(hopf_ode, [0.8*sqrt(b),0.01], lambda u0: u0[0]-sqrt(b), 2*pi, (b,a))
+			return sol[0]
 
-"""
+		numerical_sol = []
+		analytical_sol =[]
+		for i in [int(x) for x in np.linspace(b0,bf,50)]:	
+			numerical_sol.append(simulation(i,-1))
+			analytical_sol.append(sqrt(i))
 
+		sol1 = np.isclose(numerical_sol, analytical_sol, atol=1e-08)
+		sol2 = np.allclose(numerical_sol,analytical_sol, rtol=1e-05, atol=1e-08)
 
+		print("Results using np.isclose()")
+		print(sol1)
+		print("Results using np.allclose")
+		print(sol2)
 
-class test_shootingmethod_py(unittest.TestCase):
+		return 
 
-    def test_hopf_bifurcation(self):
-
-    	def hopf_ode(u0,t,b,a):
-    		x,y = u0
-    		dxdt = bx-y+a*x*(x**2+y**2)
-    		dydt = x+b*y+a*y*(x**2+y**2)
-    		return [dxdt,dydt]
-    		
-
-
-
-
-
-
-
-
-
-
-
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+# Run Test:
+test_hopf_bifurcation(0,2)
 
 
-
+def test_hopf_bifurcation_3D():
 
 
 
@@ -59,11 +58,6 @@ class test_shootingmethod_py(unittest.TestCase):
 
 
 
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
-
-if __name__ == '__main__':
-    unittest.main()
 
 
 
